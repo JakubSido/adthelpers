ccd6328d352c1c1168b95f1d5fb87cf809e091fb
Jakub Sido <JakubSido@gmail.com>
2023-04-19 10:02:27 +0200

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='adthelpers',
     version='0.0.6',
-    packages=[''],
+    packages=['adthelpers'],
     license='MIT',
     description='',
     long_description="",
